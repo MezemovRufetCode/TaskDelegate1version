dc9f740bdac4c3b10524f62bca8d7f1911dcef44
MezemovRufetCode <rufatam@code.edu.az>
2021-10-13 00:19:08 +0400

    Add project files.

---
+++ TaskDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace TaskDelegate
+{
+    class Program
+    {
+        delegate void Checkher(int number);
+        static void Main(string[] args)
+        {
+            Checkher checkher1 = new Checkher(Find);
+            Console.WriteLine("Include test number:");
+            int num = int.Parse(Console.ReadLine());
+            checkher1.Invoke(num);
+
+        }
+        public static void Find (int num1)
+        {
+            if (CheckNegative(num1))
+            {
+                Console.WriteLine(num1 + " is negative.");
+            }
+            else
+                Console.WriteLine(num1+ " is not negative.");
+        }
+        public static bool CheckNegative (int num1)
+        {
+            return num1 < 0;
+        }
+    }
+}
